77aea9a79361864d68f3221fcedc9909f6cbf6d6
SohailSTFC <133211007+SohailSTFC@users.noreply.github.com>
2024-12-13 12:17:46 +0000

    Update TensorElementIntegrate.cpp

--- TensorElementIntegrate.cpp
+++ TensorElementIntegrate.cpp
@@ -6,17 +6,18 @@
 
 using namespace std;
 int main(){
-  const int ndim = 1;
-  int pOrder = 6;
-  int nsamples = 5;
+  const int ndim = 3;
+  int pOrder = 4;
+  int nsamples = 3;
   LegendreTensorElement<float,int> MyRefElm(pOrder, nsamples, ndim);
 
   for(int I=0; I<nsamples; I++){
-  //  for(int J=0; J<nsamples; J++){
-    //  int Igauss[ndim] = {I,J};
-      int Igauss[ndim] = {I};
-      cout <<  setw(10) << MyRefElm.Get_wi(Igauss) << endl;
-   // }
+    for(int J=0; J<nsamples; J++){
+      for(int K=0; K<nsamples; K++){
+        int Igauss[ndim] = {I,J,K};
+        cout <<  setw(10) << MyRefElm.Get_xi(Igauss) << endl;
+      }
+    }
   }
 
   return 0;
